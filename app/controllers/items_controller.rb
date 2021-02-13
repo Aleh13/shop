679df6a91576cf5eba9436843a99df3ed874531d
Aleh13 <q4wk13@gmail.com>
2021-02-13 15:33:31 +0300

    Паршиалы

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,77 @@
+class ItemsController < ApplicationController
+  layout false
+  skip_before_action :verify_authenticity_token
+  before_action :find_item, only: %i[show edit update destroy upvote]
+  before_action :admin?, only: %i[edit]
+  after_action :show_info, only: %i[index]
+
+
+  def upvote
+    @item.increment! :votes_count
+    redirect_to items_path
+  end
+
+  def expensive
+    @items = Item.where('price > 50')
+    render :index
+  end
+
+
+  def index
+    @items = Item.all
+  end
+
+  def create
+    item = Item.create (items_params)
+    if item.persisted?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  #def new; end
+
+  #def show; end
+
+  #def edit; end
+
+
+  def update
+    if @item.update(items_params)
+    redirect_to item_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+
+
+  def destroy
+    if @item.destroy.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+
+  end
+
+
+  private
+  def items_params
+    params.permit(:name, :price, :real, :weight, :description)
+  end
+
+  def find_item
+    @item = Item.where(id: params[:id]).first
+    render_404 unless @item
+  end
+
+  def admin?
+    render_403 unless params[:admin]
+  end
+
+  def show_info
+    puts 'Index endpoint'
+  end
+end
